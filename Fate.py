f015701dfb4e633f00bce2869d8a90cda0271a68
Mr. Camarium <48600933+mrcamarium@users.noreply.github.com>
2023-10-23 18:53:44 +0200

    Add files via upload

--- Fate.py
+++ Fate.py
@@ -3,7 +3,7 @@ Installa email-validator
 pip install email-validator
 """
 import colorama, time
-from email_validator import validate_email, EmailNotValidError
+from email_validator import validate_email
 from colorama import Fore #BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
 from colorama import Style #DIM, NORMAL, BRIGHT, RESET_ALL
 # Variabili
@@ -14,20 +14,20 @@ rosso = Fore.LIGHTRED_EX # Colora Rosso e rende il testo brillante
 opaco = Style.DIM
 brillante = Style.BRIGHT
 # Info
-print(blu + brillante + "Version 0.1.2" + reset)
+print(blu + brillante + "Version 0.1.3" + reset)
 print(blu + opaco + "Codice by - Mr. Camarium")
 print(blu + "Youtube - mrcamarium" + reset)
 # Pausa
 time.sleep(1)
 #Verifica validità dominio dell'indirizzo e-mail
-def verificaDominio():
+def verifica_Dominio():
     while True:
-        testEmail = input("Inserisci Email da controllare: ")
+        email = input("Inserisci Email da controllare: ")
         try:
-            validate_email(testEmail)
+            validate_email(email)
             print(verde + 'Dominio Valido' + reset)
-        except EmailNotValidError as errorMsg:
-            print(rosso + str(errorMsg) + reset)
+        except:
+            print(rosso + "Dominio Non Valido" + reset)
         scelta = input("Vuoi verificare un altro indirizzo? Scegliere y/n > ")
         if (scelta == 'n'):
             print("Uscita in corso...")
@@ -35,4 +35,4 @@ def verificaDominio():
             time.sleep(1)
             break
 #main
-verificaDominio()
+verifica_Dominio()
