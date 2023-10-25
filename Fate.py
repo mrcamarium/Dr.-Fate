a6664209aaec9b6d358256ab006cd271ae523bdb
Mr. Camarium <48600933+mrcamarium@users.noreply.github.com>
2023-10-25 07:15:37 +0200

    Add files via upload

--- Fate.py
+++ Fate.py
@@ -17,7 +17,7 @@ opaco = Style.DIM
 brillante = Style.BRIGHT
 giallo = Fore.YELLOW
 #Info
-print(blu + brillante + "Version 0.4.3" + reset)
+print(blu + brillante + "Version 1.4.4" + reset)
 print(blu + opaco + "Codice by - Mr. Camarium")
 print(blu + "Youtube - mrcamarium" + reset)
 time.sleep(1) #Pausa
@@ -40,17 +40,25 @@ def verifica_esistenza(email): #Definisco una funzione che controlla l'esistenza
     return codice == 250 #Restituisco True se il codice è 250 (OK)
   except:
     return False #Se il codice è diverso, restituisco False
-email = input("Inserisci l'indirizzo email da verificare: ") #Chiedo all'utente di inserire un indirizzo email da verificare
-print("Controllo in corso...")
-if verifica_sintassi(email): #Controllo la sintassi dell'email
-  print(verde + "L'email ha una sintassi valida.")
-  if verifica_dominio(email): #Controllo il dominio dell'email
-   print(verde + 'Dominio Valido' + reset)
-   if verifica_esistenza(email): #Controllo l'esistenza dell'email
-    print(verde + "L'email esiste ed è funzionante." + reset)
+def main(): #Definisco una funzione main per il loop
+ while True:
+   email = input("Inserisci l'indirizzo email da verificare: ") #Chiedo all'utente di inserire un indirizzo email da verificare
+   print("Controllo in corso...")
+   if verifica_sintassi(email): #Controllo la sintassi dell'email
+    print(verde + "L'email ha una sintassi valida.")
+    if verifica_dominio(email): #Controllo il dominio dell'email
+     print(verde + 'Dominio Valido' + reset)
+     if verifica_esistenza(email): #Controllo l'esistenza dell'email
+      print(verde + "L'email esiste ed è funzionante." + reset)
+     else:
+      print(rosso + "L'email non esiste o non è funzionante. " + giallo + "(PS alcuni provider disabilitano VRFY e EXPN)" + reset)
+    else:
+     print(rosso + "Dominio Inesistente" + reset)
    else:
-    print(rosso + "L'email non esiste o non è funzionante. " + giallo + "(PS alcuni provider disabilitano VRFY e EXPN)" + reset)
-  else:
-   print(rosso + "Dominio Inesistente" + reset)
-else:
-  print(rosso + "L'email ha una sintassi non valida." + reset)
+    print(rosso + "L'email ha una sintassi non valida." + reset)
+   scelta = input("Vuoi verificare un altro indirizzo? Scegliere y/n > ")
+   if (scelta == 'n'):
+      print("Uscita in corso...")
+      time.sleep(1) #Pausa
+      break
+main() #main
