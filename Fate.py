c296198c4f489f7d9097ad2c86aa0e4d11e033a0
Mr. Camarium <48600933+mrcamarium@users.noreply.github.com>
2023-10-27 17:32:07 +0200

    Add files via upload

--- Fate.py
+++ Fate.py
@@ -4,7 +4,7 @@ Dal momento che viene spesso usato per scopi di intrusione e spam, molte societ�
 default.
 """
 # Importo le librerie
-import smtplib, colorama, time, re
+import smtplib, colorama, time, re, sys, socket
 from email_validator import validate_email
 from colorama import Fore #BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE
 from colorama import Style #DIM, NORMAL, BRIGHT, RESET_ALL
@@ -17,9 +17,13 @@ opaco = Style.DIM
 brillante = Style.BRIGHT
 giallo = Fore.YELLOW
 #Info
-print(blu + brillante + "Version 1.4.4" + reset)
+print(blu + brillante + "Version 1.7.4" + reset)
 print(blu + opaco + "Codice by - Mr. Camarium")
-print(blu + "Youtube - mrcamarium" + reset)
+print(blu + "Youtube - mrcamarium" + reset, '\n')
+hostname = socket.gethostname()
+ipAddress = socket.gethostbyname(hostname)
+print(f"Hostname: {hostname}")
+print('Il mio indirizzo IP è: ', ipAddress,'\n')
 time.sleep(1) #Pausa
 def verifica_sintassi(email): #Definisco una funzione che controlla la sintassi di un indirizzo email
   regex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$" #Uso una regex per verificare che l'email abbia il formato corretto
@@ -40,8 +44,29 @@ def verifica_esistenza(email): #Definisco una funzione che controlla l'esistenza
     return codice == 250 #Restituisco True se il codice è 250 (OK)
   except:
     return False #Se il codice è diverso, restituisco False
-def main(): #Definisco una funzione main per il loop
- while True:
+def menu(): #Menu
+    print(giallo + """
+\t 1. Verifica Indirizzo Email
+\t 2. Verifica Indirizzo IP
+\t 3. Null
+\t 4. Null
+\t 5. Null
+\t 6. Null
+\t 7. Exit
+""" + reset)
+def control(): #Lista azioni
+    ctrl = input("Effettua La Scelta: ")
+    if ctrl == "1" :
+        veremail() #Controllo indirizzo email
+    elif ctrl == "2" :
+        ipinfo() #Controllo indirizzo IP
+    elif ctrl == "3" :
+        veremail()
+    elif ctrl == "7" :
+        sys.exit()
+    else :
+        print(rosso + "Scelta Errata" + reset)    
+def veremail(): #Verifico Email
    email = input("Inserisci l'indirizzo email da verificare: ") #Chiedo all'utente di inserire un indirizzo email da verificare
    print("Controllo in corso...")
    if verifica_sintassi(email): #Controllo la sintassi dell'email
@@ -56,9 +81,12 @@ def main(): #Definisco una funzione main per il loop
      print(rosso + "Dominio Inesistente" + reset)
    else:
     print(rosso + "L'email ha una sintassi non valida." + reset)
-   scelta = input("Vuoi verificare un altro indirizzo? Scegliere y/n > ")
-   if (scelta == 'n'):
-      print("Uscita in corso...")
-      time.sleep(1) #Pausa
-      break
-main() #main
+def ipinfo(): #Verifico IP
+    import webbrowser
+    new = 2
+    url = ("https://ipinfo.io/")
+    term = input("Inserisci Indirizzo IP: ")
+    webbrowser.open(url+term,new=new)
+while True: #Ricomincia il programma
+ menu()
+ control()
